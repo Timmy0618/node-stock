cda7ade5f044ff6a95dd9f7f40fdb0a959e6a301
Timmy <timmy85618@gmail.com>
2021-02-19 19:32:09 +0800

    api 優化

--- test.js
+++ test.js
@@ -2,23 +2,30 @@ const fetch = require('node-fetch');
 
 
 
-function get_stock_data(stockname) {
+async function get_stock_data(stockname) {
     var time = get_time();
-    return fetch('https://query1.finance.yahoo.com/v8/finance/chart/'+stockname+'?symbol='+stockname+'&period1='+time.before+'&period2='+time.now+'&useYfid=true&interval=1d&includePrePost=true&events=div%7Csplit%7Cearn&lang=en-US&region=US&crumb=s9eQSRza8b8&corsDomain=finance.yahoo.com')
+    return await fetch('https://query1.finance.yahoo.com/v8/finance/chart/' + stockname + '?symbol=' + stockname + '&period1=' + time.before + '&period2=' + time.now + '&useYfid=true&interval=1d&includePrePost=true&events=div%7Csplit%7Cearn&lang=en-US&region=US&crumb=s9eQSRza8b8&corsDomain=finance.yahoo.com')
         .then(function (data) {
             return data.json();
         })
 }
 
-function get_time(){
+function get_time() {
     var today = new Date();
-    var now = Number(Date.parse(today))/1000;
-    var before = (now - 5*365*86400);
-    var time = {now:now,before:before};
+    var now = Number(Date.parse(today)) / 1000;
+    var before = (now - 5 * 365 * 86400);
+    var time = { now: now, before: before };
     console.log(time);
     return time;
 }
 
 module.exports = get_stock_data;
+async function main() {
+    var data = await get_stock_data('INTC');
+    var timestamp = data.chart.result[0].timestamp;
+    var stock = data.chart.result[0].indicators.quote[0];
+    stock.timestamp = timestamp;
+    console.log(stock);
+}
 
-get_time();
+main();
