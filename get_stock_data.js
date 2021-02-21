5bb281d88426d6b0b422477d55bf99fefa442976
Timmy_DESK <timmy85618@gmail.com>
2021-02-21 23:21:44 +0800

    新增表格塞選

--- get_stock_data.js
+++ get_stock_data.js
@@ -4,11 +4,14 @@ const fetch = require('node-fetch');
 
 async function get_stock_data(stockname) {
     var time = get_time();
+    console.log(stockname);
     return await fetch('https://query1.finance.yahoo.com/v8/finance/chart/' + stockname + '?symbol=' + stockname + '&period1=' + time.before + '&period2=' + time.now + '&useYfid=true&interval=1d&includePrePost=true&events=div%7Csplit%7Cearn&lang=en-US&region=US&crumb=s9eQSRza8b8&corsDomain=finance.yahoo.com')
         .then(function (data) {
             return data.json();
         })
         .then(function (data) {
+            if (data.length ===0)
+                return data;
             var timestamp = data.chart.result[0].timestamp;
             var stock = data.chart.result[0].indicators.quote[0];
             stock.timestamp = timestamp;
